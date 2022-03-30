8cf12482db697432f5d5ebe4acc6e4eb1571b9e7
skaffa <59300989+skaffa@users.noreply.github.com>
2022-03-30 07:53:22 -0700

    changes

--- interface.py
+++ interface.py
@@ -1,13 +1,10 @@
 print('ip-tools cli')
 print('')
 print('1. melissa')
-print('2. HTML-fetch (WIP)')
-print('3. shodan (WIP)')
-print('4. intelx (WIP)')
+print('2. shodan (WIP)')
+print('3. intelx (WIP)')
+print('4. html-fetch (WIP)')
 print('5. convert (WIP)')
-print('6. ip-lookup')
-print('7. ip-lookup')
-print('8. ip-lookup')
 
 
 
@@ -18,6 +15,10 @@ selection = input("Enter module:\n>")
 
 if selection == "1":
     exec(open("melissa.py").read())
+if selection == "2":
+    exec(open("shodan.py").read())
+if selection == "3":
+    exec(open("shodan.py").read())
 
 else:
     print('Invalid input')
